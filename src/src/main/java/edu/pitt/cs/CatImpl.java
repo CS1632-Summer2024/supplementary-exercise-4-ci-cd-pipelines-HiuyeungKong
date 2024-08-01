a7467440d8d7a7df99739d08abc49542999c700a
Hiuyeung Kong <hik40@pitt.edu>
2024-08-01 06:25:33 -0400

    commit

---
+++ src/src/main/java/edu/pitt/cs/CatImpl.java
@@ -0,0 +1,53 @@
+package edu.pitt.cs;
+
+public class CatImpl implements Cat {
+
+	// TODO: Fill in with member variables
+	String name;
+	int id;
+	Boolean rented;
+
+	public CatImpl(int id, String name) {
+		// TODO: Fill in
+		this.id = id;
+		this.name = name;
+		this.rented = false;
+	}
+
+	public void rentCat() {
+		// TODO: Fill in
+		rented = true;
+	}
+
+	public void returnCat() {
+		// TODO: Fill in
+		rented = false;
+	}
+
+	public void renameCat(String name) {
+		// TODO: Fill in
+		this.name = name;
+	}
+
+	public String getName() {
+		// TODO: Fill in
+		return name;
+	}
+
+	public int getId() {
+		// TODO: Fill in
+		return id;
+	}
+
+	public boolean getRented() {
+		// TODO: Fill in
+		return rented;
+	}
+
+	public String toString() {
+		// TODO: Fill in
+		String ret = "ID " + id + ". " + name;
+		return ret;
+	}
+
+}
